9d237b4bff8259543cce615357d35e13b2c4cdc1
Jay <jay@tangram.zone>
2023-02-23 17:22:49 +0800

    feat: add tailwind

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -122,6 +122,19 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+    plugins:[
+      async function tailwindcssPlugin(context, options) {
+        return {
+          name: "docusaurus-tailwindcss",
+          configurePostCss(postcssOptions) {
+            // Appends TailwindCSS and AutoPrefixer.
+            postcssOptions.plugins.push(require("tailwindcss"));
+            postcssOptions.plugins.push(require("autoprefixer"));
+            return postcssOptions;
+          },
+        };
+      },
+    ]
 };
 
 module.exports = config;
